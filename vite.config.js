7743c42b44290b3b4b04f8b686eb9b661b4b8b2b
Georgi Markov <gogata1905@gmail.com>
2024-08-05 02:17:02 +0300

    add Tests with Vitest Testing Framework

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
-// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,7 +31,6 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,png,jpg,}"],
       },
     }),
-    // visualizer({ open: true }),
   ],
   resolve: {
     alias: {
